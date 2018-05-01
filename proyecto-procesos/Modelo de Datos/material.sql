7c83c83b1380a7e2ee79f50999db576d37bf25f0
JaniceStefa <38575278+JaniceStefa@users.noreply.github.com>
2018-05-01 18:44:54 -0500

    Add files via upload

--- proyecto-procesos/Modelo de Datos/material.sql
+++ proyecto-procesos/Modelo de Datos/material.sql
@@ -1,25 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-04-2018 a las 03:22:48
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.0.29
+-- Tiempo de generación: 02-05-2018 a las 01:05:14
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `proyecto_garras`
+-- Base de datos: `proyecto`
 --
 
 -- --------------------------------------------------------
@@ -28,12 +26,12 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `material`
 --
 
-CREATE TABLE `material` (
+CREATE TABLE IF NOT EXISTS `material` (
   `cod_material` varchar(7) NOT NULL,
   `Descripcion` varchar(20) NOT NULL,
-  `Precio` decimal(6,2) NOT NULL
+  `Precio` decimal(6,2) NOT NULL,
+  `Estado` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

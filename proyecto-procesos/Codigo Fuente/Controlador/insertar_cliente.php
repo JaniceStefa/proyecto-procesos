5f0b1b2938d3e378b08586c4bfeec822fa883678
GinoTaipe <gino_gjtm@hotmail.com>
2018-05-07 12:05:33 -0500

    Add files via upload

--- proyecto-procesos/Codigo Fuente/Controlador/insertar_cliente.php
+++ proyecto-procesos/Codigo Fuente/Controlador/insertar_cliente.php
@@ -1,4 +1,5 @@
 <?php 
+	require_once("../Modelo/M_registrar_cliente.php");
 	$DNI=$_POST['dni'];
 	$ApellidoPaterno=$_POST['apPaterno'];
 	$ApellidoMaterno=$_POST['apMaterno'];
@@ -9,8 +10,7 @@
 	$Contraseña=$_POST['contraseña'];
 	$TipoAcceso=$_POST['tipoacceso'];
 	$Estado=$_POST['estado'];
-
-	require_once("../Modelo/M_registrar_cliente.php");
 	$clientes=new M_registrar_cliente();
 	$clientes->InsertarCliente($DNI,$ApellidoPaterno,$ApellidoMaterno,$Nombres,$Direccion,$Telefono,$Email,$Contraseña,$TipoAcceso,$Estado);
- ?>
+	
+	?>
